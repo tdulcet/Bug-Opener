c9730288f7f693d55d3cd594e14dd419eb0a1db4
Teal Dulcet <tdulcet@pdx.edu>
2022-08-15 02:27:53 -0700

    Added GitHub Actions CI.

--- background.js
+++ background.js
@@ -247,7 +247,7 @@ const getURLs = Object.freeze({
  * Delay.
  *
  * @param {number} delay
- * @returns {Promise}
+ * @returns {Promise<void>}
  */
 function delay(delay) {
 	return new Promise((resolve) => {
